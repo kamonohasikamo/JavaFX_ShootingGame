199fd47b03d2fbe75d15511e2757975b87cd198f
kamonohasikamo <arrowsuke@yahoo.co.jp>
2020-08-01 12:24:44 +0900

    Playerが動くところまで

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,11 +1,13 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
@@ -16,6 +18,13 @@ public class Main extends Application {
     static final int WINDOW_WIDTH = 1000;
     static final int WINDOW_HEIGHT = 600;
     static final Color BACKGROUNDCOLOR = Color.BLACK;
+    static final int KEY_RIGHT = 0;
+    static final int KEY_LEFT  = 1;
+    static final int KEY_UP    = 2;
+    static final int KEY_DOWN  = 3;
+    byte[] key = new byte[4];
+    double player_x = 100;
+    double player_y = 100;
 
     @Override
     public void start(Stage theStage) {
@@ -37,9 +46,17 @@ public class Main extends Application {
         theStage.show();
         Thread gameThread = new Thread(() -> {
             while(true) {
-                gc.drawImage(player, 50, 50);
+                // 全画面の消去処理で再描画
+                gc.clearRect(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+
+                // Pleyer の座標更新処理
+                player_x += (key[KEY_RIGHT] - key[KEY_LEFT]);
+                player_y += (key[KEY_DOWN] - key[KEY_UP]);
+
+                // その位置に描画
+                gc.drawImage(player, player_x, player_y, 30, 30);
                 try {
-                    Thread.sleep(2);
+                    Thread.sleep(3);
                 } catch(Exception e) {
 
                 }
@@ -47,6 +64,28 @@ public class Main extends Application {
         });
         gameThread.setDaemon(true);
         gameThread.start();
+
+        // On press key
+        scene.setOnKeyPressed(e -> {
+            System.out.println("KeyCode = " + e.getCode());
+            switch (e.getCode()) {
+                case RIGHT: key[KEY_RIGHT] = 1; break;
+                case LEFT:  key[KEY_LEFT]  = 1; break;
+                case UP:    key[KEY_UP]    = 1; break;
+                case DOWN:  key[KEY_DOWN]  = 1; break;
+            }
+        });
+
+        // Release key
+        scene.setOnKeyReleased(e -> {
+            switch (e.getCode()) {
+                case RIGHT: key[KEY_RIGHT] = 0; break;
+                case LEFT:  key[KEY_LEFT]  = 0; break;
+                case UP:    key[KEY_UP]    = 0; break;
+                case DOWN:  key[KEY_DOWN]  = 0; break;
+            }
+        });
+
     }
 
     public static void main(String[] args) {
